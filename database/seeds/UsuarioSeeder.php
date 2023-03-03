d7b6e740aeebddaf4bda19c91ab64ee63a1c31d1
Lucas Henrique <lucasmonte2011@gmail.com>
2023-03-02 23:08:37 -0300

    Adicionando usuário adm nos seeders

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -13,130 +13,131 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
+
         DB::table('users')->insert([
 
-          'name'=>'Administrador',
-          'email'=>'admin@ufrpe.br',
+          'name'=>'Prograd',
+          'email'=>'editais.prograd@upe.br',
           'password'=>Hash::make('12345678'),
           'tipo'=>'administrador',
-          'email_verified_at'=>'2020-01-01'
+          'email_verified_at'=>'2022-01-01'
         ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Lucas Henrique (ADM)',
-          'email'=>'lucas.henriquemonteiro@upe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'administrador',
-          'email_verified_at'=>'2023-01-01'
-        ]);
+        //   'name'=>'Administrador',
+        //   'email'=>'admin@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'administrador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'AdministradorResponsavel1',
-          'email'=>'adminResp1@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'administradorResponsavel',
-          'email_verified_at'=>'2020-01-01'
-        ]);
-        DB::table('users')->insert([
+        //   'name'=>'Lucas Henrique (ADM)',
+        //   'email'=>'lucas.henriquemonteiro@upe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'administrador',
+        //   'email_verified_at'=>'2023-01-01'
+        // ]);
 
-          'name'=>'AdministradorResponsavel2',
-          'email'=>'adminResp2@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'administradorResponsavel',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        // DB::table('users')->insert([
 
-        DB::table('users')->insert([
+        //   'name'=>'AdministradorResponsavel1',
+        //   'email'=>'adminResp1@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'administradorResponsavel',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
+        // DB::table('users')->insert([
 
-          'name'=>'Proponente',
-          'email'=>'usuario@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'proponente',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'AdministradorResponsavel2',
+        //   'email'=>'adminResp2@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'administradorResponsavel',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Gabriel',
-          'email'=>'gabriel.uag.ufrpe@gmail.com',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'proponente',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Proponente',
+        //   'email'=>'usuario@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'proponente',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Coordenador1',
-          'email'=>'coordenador1@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'coordenador',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Coordenador1',
+        //   'email'=>'coordenador1@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'coordenador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Coordenador2',
-          'email'=>'coordenador2@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'coordenador',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Coordenador2',
+        //   'email'=>'coordenador2@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'coordenador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Participante1',
-          'email'=>'part1@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'participante',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Participante1',
+        //   'email'=>'part1@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'participante',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Participante2',
-          'email'=>'part2@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'participante',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Participante2',
+        //   'email'=>'part2@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'participante',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Avaliador1',
-          'email'=>'aval1@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'avaliador',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Avaliador1',
+        //   'email'=>'aval1@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'avaliador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Avaliador2',
-          'email'=>'aval2@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'avaliador',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Avaliador2',
+        //   'email'=>'aval2@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'avaliador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Avaliador3',
-          'email'=>'aval3@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'avaliador',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Avaliador3',
+        //   'email'=>'aval3@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'avaliador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
 
-          'name'=>'Avaliador4',
-          'email'=>'aval4@ufrpe.br',
-          'password'=>Hash::make('12345678'),
-          'tipo'=>'avaliador',
-          'email_verified_at'=>'2020-01-01'
-        ]);
+        //   'name'=>'Avaliador4',
+        //   'email'=>'aval4@ufrpe.br',
+        //   'password'=>Hash::make('12345678'),
+        //   'tipo'=>'avaliador',
+        //   'email_verified_at'=>'2020-01-01'
+        // ]);
 
     }
 }
