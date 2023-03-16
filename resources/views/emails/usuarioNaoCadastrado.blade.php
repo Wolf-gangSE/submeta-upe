898ac964a5ead5b8c6622a2f282888d93d2aabde
Lucas Henrique <lucasmonte2011@gmail.com>
2023-03-16 20:10:12 -0300

    Adiciona instruções no e-mail para os avaliadores

--- resources/views/emails/usuarioNaoCadastrado.blade.php
+++ resources/views/emails/usuarioNaoCadastrado.blade.php
@@ -11,6 +11,8 @@
 			Agradecemos a sua disponibilidade para participar do banco de avaliadores de propostas de @if($natureza == '1') Ensino @elseif($natureza=='2') Pesquisa @elseif($natureza == '3') Extensão @endif
 			do {{$nomeEvento}} da Universidade de Pernambuco (UPE).
 			<br><br>Solicitamos, gentilmente, que acesse o sistema Submeta através do <a href="{{ url('/') }}">LINK</a> e da senha {{$senhaTemporaria}}, para realizar o login no sistema e o seu aceite como avaliador de propostas de projetos @if($natureza == '3')de Extensão e Cultura @elseif($natureza == '1') Ensino @elseif($natureza == '2') Pesquisa @endif da UPE.
+
+			<br><br>Por questões procedimentais, gostaríamos de solicitar que Vossa Senhoria faça o login no sistema Submeta e decida se deseja aceitar ou recusar o convite para se tornar um avaliador do edital {{$nomeEvento}}.
 			
 			@if($natureza == '3')
 				<br><br>Desde já, agradecemos a disponibilidade de participar do banco de avaliadores Ad hoc de propostas de Extensão e Cultura da UPE.
@@ -43,6 +45,8 @@
 			Agradecemos a sua disponibilidade para participar do banco de avaliadores de propostas de @if($natureza == '1') Ensino @elseif($natureza=='2') Pesquisa @elseif($natureza == '3') Extensão @endif
 			do {{$nomeEvento}} da Universidade de Pernambuco (UPE).
 			<br><br>Solicitamos, gentilmente, que acesse o sistema Submeta através do <a href="{{ url('/') }}">LINK</a>, para realizar o login no sistema e o seu aceite como avaliador de propostas de projetos @if($natureza == '3')de Extensão e Cultura @elseif($natureza == '1') Ensino @elseif($natureza == '2') Pesquisa @endif da UPE.
+
+			<br><br>Por questões procedimentais, gostaríamos de solicitar que Vossa Senhoria faça o login no sistema Submeta e decida se deseja aceitar ou recusar o convite para se tornar um avaliador do edital {{$nomeEvento}}.
 			
 			@if($natureza == '3')
 				<br><br>Desde já, agradecemos a disponibilidade de participar do banco de avaliadores Ad hoc de propostas de Extensão e Cultura da UPE.
